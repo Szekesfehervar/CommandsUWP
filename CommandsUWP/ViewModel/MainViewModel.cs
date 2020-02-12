785382deea01c783e9b6ea6c7e8af9441382d3be
Pepa <josef.simunek@pslib.cz>
2020-02-12 09:42:06 +0100

    Add project files.

---
+++ CommandsUWP/ViewModel/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandsUWP.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private int _number;
+        private int _max;
+        private Random _random;
+
+        public MainViewModel()
+        {
+            _random = new Random();
+            Roll = new RelayCommand(
+                () => { Number = _random.Next(Max); },
+                () => { return (Max > 0 && Max <= 6); });
+
+            SetMax = new ParametrizedRelayCommand(
+                (parameter) =>
+                {
+                    if (Int32.TryParse(parameter as string, out int result))
+                    { Max = result; }
+                    if (Roll.CanExecute(null)) Roll.Execute(null);
+                });
+        }
+
+        public int Number
+        {
+            get
+            {
+                return _number +1;
+            }
+            set
+            {
+                _number = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public int Max
+        {
+            get
+            {
+                return _max;
+            }
+            set
+            {
+                _max = value;
+                Roll.RaiseCanExcuteChanged();
+                NotifyPropertyChanged();
+            }
+        }
+
+        public RelayCommand Roll { get; set; }
+    }
+}
